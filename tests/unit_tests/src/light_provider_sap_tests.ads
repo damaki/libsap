75aec30f860ff91de7b18cdb28cd991beb823f29
Daniel King <damaki.gh@gmail.com>
2026-02-27 20:52:36 +0000

    Add testsuite for the light SAP packages

---
+++ tests/unit_tests/src/light_provider_sap_tests.ads
@@ -0,0 +1,17 @@
+--
+--  Copyright 2026 (C) Daniel King
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+with AUnit.Test_Fixtures;
+with AUnit.Test_Suites;
+
+package Light_Provider_SAP_Tests is
+
+   type Test is new AUnit.Test_Fixtures.Test_Fixture with null record;
+
+   procedure Test_One_Normal_Transaction (T : in out Test);
+
+   function Suite return AUnit.Test_Suites.Access_Test_Suite;
+
+end Light_Provider_SAP_Tests;
